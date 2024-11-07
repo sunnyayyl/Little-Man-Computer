b3c4d7c4f6d1ed6c68b50f57eaa66ee71f31b5dc
sunnyayyl <sunny.ayyl@gmail.com>
2024-11-07 23:17:00 +0000

    Test

---
+++ cross_compile_test/x86_64-pc-windows-gnu/debug/deps/LittleManComputer-77ef569e1d7aa5df.d
@@ -0,0 +1,8 @@
+/var/home/sunnyayyl/RustroverProjects/LittleManComputer/cross_compile_test/x86_64-pc-windows-gnu/debug/deps/LittleManComputer-77ef569e1d7aa5df.exe: src/main.rs src/assembler.rs src/opcodes.rs src/vm.rs
+
+/var/home/sunnyayyl/RustroverProjects/LittleManComputer/cross_compile_test/x86_64-pc-windows-gnu/debug/deps/LittleManComputer-77ef569e1d7aa5df.d: src/main.rs src/assembler.rs src/opcodes.rs src/vm.rs
+
+src/main.rs:
+src/assembler.rs:
+src/opcodes.rs:
+src/vm.rs:
