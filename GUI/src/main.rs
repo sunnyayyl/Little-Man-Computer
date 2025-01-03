857d2ea7d9d7e30c2d2e9266549c777b83461d1d
sunnyayyl <sunny.ayyl@gmail.com>
2025-01-03 23:07:24 +0000

    Early gtk implementation

--- GUI/src/main.rs
+++ GUI/src/main.rs
@@ -1,7 +1,10 @@
-use adw::glib::clone;
+use adw::{gio, glib, Application};
 use adw::prelude::*;
-use adw::{gio, glib, Application, HeaderBar};
-use gtk::{Box, Button, Orientation};
+use crate::window::Window;
+
+mod window;
+
+
 fn main() -> glib::ExitCode {
     gio::resources_register_include!("compiled.gresource")
         .expect("Failed to register resources.");
@@ -11,49 +14,7 @@ fn main() -> glib::ExitCode {
     app.connect_activate(build_ui);
     app.run()
 }
-fn build_ui(application: &Application) {
-    let button = Button::builder()
-        .label("Click me!")
-        .margin_top(12)
-        .margin_bottom(12)
-        .margin_start(12)
-        .margin_end(12)
-        .build();
-    let undo = Button::builder()
-        .label("Undo")
-        .margin_bottom(12)
-        .margin_start(12)
-        .margin_end(12)
-        .build();
-    button.connect_clicked(clone!(
-        move |button| {
-            button.set_label("Clicked!");
-        }
-    ));
-    undo.connect_clicked(clone!(
-        #[weak]
-        button,
-        move |_| {
-            button.set_label("Click me!");
-        }
-    ));
-    let body = gtk::Box::builder()
-        .orientation(Orientation::Vertical)
-        .build();
-    body.append(&button);
-    body.append(&undo);
-    let content = Box::new(Orientation::Vertical, 0);
-    content.append(&HeaderBar::new());
-    content.append(&body);
-    let window = adw::ApplicationWindow::builder()
-        .application(application)
-        .default_width(320)
-        .default_height(200)
-        .content(&content)
-        .title("Hello, World!")
-        .build();
+pub fn build_ui(app: &Application){
+    let window=Window::new(app);
     window.present();
 }
-
-
-
